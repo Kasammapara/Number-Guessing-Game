dd9f6fa90e44607ca3e327e49590b19889686c50
Kasammapara <99660209+Kasammapara@users.noreply.github.com>
2022-02-14 18:32:34 +0530

    Create Guess.py

---
+++ Guess.py
@@ -0,0 +1,10 @@
+secret_no = 3
+no_guess = 0
+while no_guess < 3 :
+    guess = int(input(Guess The Number: ))
+    no_guess += 1
+    if guess == secret_no :
+      print("You Won!")
+    break
+else:
+  print("Sorry, You Lose!")
